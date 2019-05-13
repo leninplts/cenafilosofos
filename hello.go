0b2858ab764de24ca5f4848bc74036e95e419ee6
Lenin Jose <paltokj@gmail.com>
2019-05-13 00:26:47 -0500

    second code for Cena de Filosofos

--- hello.go
+++ hello.go
@@ -6,59 +6,52 @@ import (
 	"time"
 )
 
-type fork struct{ sync.Mutex }
+type palillo struct{ sync.Mutex }
 
-type philosopher struct {
-	id                  int
-	leftFork, rightFork *fork
+type filosofo struct {
+	id                               int
+	palilloIzquierdo, palilloDerecho *palillo
 }
 
-// Goes from thinking to hungry to eating and done eating then starts over.
-// Adapt the pause values to increased or decrease contentions
-// around the forks.
-func (p philosopher) eat() {
+func (f filosofo) comer() {
 	for j := 0; j < 3; j++ {
-		p.leftFork.Lock()
-		p.rightFork.Lock()
+		f.palilloIzquierdo.Lock()
+		f.palilloDerecho.Lock()
 
-		say("eating", p.id)
+		decir("comenzando a comer: ", f.id)
 		time.Sleep(time.Second)
 
-		p.rightFork.Unlock()
-		p.leftFork.Unlock()
+		f.palilloDerecho.Unlock()
+		f.palilloIzquierdo.Unlock()
 
-		say("finished eating", p.id)
+		decir("terminando de comer: ", f.id)
 		time.Sleep(time.Second)
 	}
-	eatWgroup.Done()
+	comerEnGrupo.Done()
 }
 
-func say(action string, id int) {
-	fmt.Printf("Philosopher #%d is %s\n", id+1, action)
+func decir(action string, id int) {
+	fmt.Printf("filosofo #%d esta %s\n", id+1, action)
 }
 
-var eatWgroup sync.WaitGroup
+var comerEnGrupo sync.WaitGroup
 
 func main() {
-	// How many philosophers and forks
+	cont := 5
 
-	count := 5
-
-	// Create forks
-	forks := make([]*fork, count)
-	for i := 0; i < count; i++ {
-		forks[i] = new(fork)
+	forks := make([]*palillo, cont)
+	for i := 0; i < cont; i++ {
+		forks[i] = new(palillo)
 	}
 
-	// Create philospoher, assign them 2 forks and send them to the dining table
-	philosophers := make([]*philosopher, count)
-	for i := 0; i < count; i++ {
-		philosophers[i] = &philosopher{
-			id: i, leftFork: forks[i], rightFork: forks[(i+1)%count]}
-		eatWgroup.Add(1)
-		go philosophers[i].eat()
+	filosofos := make([]*filosofo, cont)
+	for i := 0; i < cont; i++ {
+		filosofos[i] = &filosofo{
+			id: i, palilloIzquierdo: forks[i], palilloDerecho: forks[(i+1)%cont]}
+		comerEnGrupo.Add(1)
+		go filosofos[i].comer()
 
 	}
-	eatWgroup.Wait()
+	comerEnGrupo.Wait()
 
 }
